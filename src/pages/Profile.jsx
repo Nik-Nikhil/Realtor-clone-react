cd7574ab402c964497bd5e3bf17f07074bcb31e7
Nikhil Singh <56159827+Nik-Nikhil@users.noreply.github.com>
2024-01-05 16:18:49 +0530

    created a private Route to protect the profile page and logout functionnally

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { getAuth } from "firebase/auth"
+import { useState } from "react"
+import { useNavigate } from "react-router"
 
-const Profile = () => {
+export default function Profile ()  {
+
+  const auth = getAuth()
+  const navigate = useNavigate()
+
+
+  const [formData, setFormData] = useState({
+    name:auth.currentUser.displayName,
+    email:auth.currentUser.email,
+  })
+  function onLogOut() {
+    auth.signOut()
+    navigate("/")
+  }
+  const {name, email} = formData
+
   return (
-    <div>Profile</div>
+    <>
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+      <h1 className='text-3xl text-center mt-6 font-bold'>My Profile</h1>
+      <div className="w-full md:w-[50%] mt-6 px-3">
+        <form>
+          { /* Name  Input*/ }
+          <input type='text' id="name" value={name} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded ease-in-out transition"/>
+
+          { /* Email Input*/ }
+          <input type='email' id="email" value={email} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded ease-in-out transition"/>
+
+          <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+            <p className="flex items-center mb-6">Do you want to change your name?
+              <span className="text-orange-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">Edit</span>
+            </p>
+            <p onClick={onLogOut} className="text-indigo-600 hover:text-indigo-700 transition duration-200 ease-in-out cursor-pointer">Sign out</p>
+          </div>
+          
+
+
+        </form>
+      </div>
+    </section>
+    </>
   )
 }
 
-export default Profile
